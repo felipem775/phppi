4745f9c5cf4b7023917a614cd40fcbcab3d3b193
Felipe Maza <felipe@felipem.com>
2012-12-21 20:15:23 +0100

    Añadido gráfico con espacio en USB

--- index.php
+++ index.php
@@ -30,20 +30,45 @@
 				?>
 			]);
       
-		        // Create and draw the visualization.
 			new google.visualization.LineChart(document.getElementById('visualizacionTemperatura')).
 				draw(data, {curveType: "function",
 				width: 700, height: 400,
 				vAxis: {minValue: 37, maxValue: 43}}
 			);
     }
-      
+
+	    
+		function graficoEspacioUSB() {
+			<?php 
+				$commandocupado = "df -h |grep usb|awk '{printf(\"%d\",$3);}'";
+				$arrayocupado = explode("\n", shell_exec($commandocupado));
+				$ocupado = $arrayocupado[0];
+				
+				$commandlibre = "df -h |grep usb|awk '{printf(\"%d\",$4);}'";
+				$arraylibre = explode("\n", shell_exec($commandlibre));
+				$libre = $arraylibre[0];
+			?>
+			var data = new google.visualization.DataTable();
+			data.addColumn('string', 'Topping');
+	    data.addColumn('number', 'Slices');
+	    data.addRows([
+				['Ocupado', <?php echo $ocupado?>],
+        ['Libre', <?php echo $libre?>]
+      ]);
+	    var options = {'title':'Disco USB', 'width':400, 'height':300};
+			var chart = new google.visualization.PieChart(document.getElementById('visualizacionEspacioUSB'));
+      chart.draw(data, options);
+				
+		}      
 
 		google.setOnLoadCallback(graficaTemperatura);
+		google.setOnLoadCallback(graficoEspacioUSB);
 		
 	</script>
 </head>
 <body>
 	<div id="visualizacionTemperatura" style="width: 700px; height: 400px;"></div>
+	<div id="visualizacionEspacioUSB" ></div>
+	
 </body>
 </html>
